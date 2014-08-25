a44cf0a0f20806aedb9a1788658eab53f3dc40df
Pasley <pasley@gmail.com>
2014-08-24 19:32:52 -0700

    pagenation complete

--- index.php
+++ index.php
@@ -1,24 +1,19 @@
 <?php
-
 session_start();
-
 ini_set('auto_detect_line_endings', true);
-
+$ctual_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 if(isset($_GET["file_name"]))
 {
     $handle = fopen($_GET["file_name"], "r");
     $names = array();
-
     while(($data = fgetcsv($handle))) {
         $names[] = $data;
     }
-
     $csv_length = count($names);
 }
-
 function display_row($array, $start)
 {
-    for($i = $start; $i <= $start + 50; $i++)
+    for($i = $start; $i < $start + 50; $i++)
     {
         echo '<tr>';
         foreach($array[$i] as $value)
@@ -27,12 +22,20 @@ function display_row($array, $start)
         }
         echo '</tr>';
     }
+    echo '<div id=navigation>';
+        if(($start - 50) > 0)
+        {
+            $link_string='<a href="index.php?file_name=' . $_GET['file_name'] . '&row=' . ($start-50) . '">Previous Page</a>';
+            echo $link_string;
+        }
+        if(($start + 50) < count($array))
+        {
+            $link_string='<a href="index.php?file_name=' . $_GET['file_name'] . '&row=' . ($start+50) . '">Next Page</a>';
+            echo $link_string;
+        }
+    echo '</div>';
 }
-
-
 ?>
-
-
 <!DOCTYPE html>
 <html>
     <head>
@@ -46,7 +49,7 @@ function display_row($array, $start)
                 <input type="submit" value="Submit">
             </form>
 <?php
-            if(isset($_GET["file_name"]))
+            if(!empty($_GET["file_name"]) && empty($_SESSION['errors']))
             {  ?>
             <table>
                 <thead>
@@ -54,18 +57,23 @@ function display_row($array, $start)
 <?php
                         foreach($names[0] as $value)
                         {  ?>
-                        <th><?= $value ?></th>
+                            <th><?= $value ?></th>
 <?php                   }  ?>
                     </tr>
                 </thead>
                 <tbody>
 <?php
-                        display_row($names, 1);
+                    display_row($names, $_GET['row']);
 ?>
                 </tbody>
             </table>
 <?php
-            }  ?>
+            }
+            else 
+            { ?>
+                <p>Please select a file to be processed.</p>
+<?php           $_SESSION=array();         
+            } ?>
         </div>
     </body>
 </html>
--- process.php
+++ process.php
@@ -1,25 +1,25 @@
 <?php
-
 session_start();
-
-if(isset($_POST['action']) && $_POST['action'] == 'file_upload')
+if(isset($_POST['action']) && $_POST['action'] == 'file_upload' && empty($_SESSION['errors']))
 {
     upload_file($_FILES, $_SESSION);
-    header("Location: index.php?file_name=upload/" . $_FILES['file_upload']['name']);
+    header("Location: index.php?file_name=upload/" . $_FILES['file_upload']['name'] . '&row=1');
     exit;
 }
-
+else
+{
+    header("Location: index.php");
+    exit;
+}
 function upload_file($files, $session)
 {
     if(($files['file_upload']['error'])==4)
     {
-        $session['errors']['file_upload']='Please select a profile picture';
+        $_SESSION['errors']['file_upload']='No file selected for upload uploaded';
     }
     elseif($files && !isset($session['errors']))
     {
         move_uploaded_file($files['file_upload']['tmp_name'], 'upload/' . basename($files['file_upload']['name']));
     }
 }
-
-
-?>
+//---end of file
